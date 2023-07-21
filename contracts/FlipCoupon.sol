1daf23d0c9f8b5555b2d7d71363859a7ff929a59
adnaan.ahmed <adnaan.ahmed@flipkart.com>
2023-07-21 21:47:00 +0530

    added support for coupon code with burn fix

--- contracts/FlipCoupon.sol
+++ contracts/FlipCoupon.sol
@@ -5,18 +5,22 @@ import "@thirdweb-dev/contracts/base/ERC721Base.sol";
 
 contract FlipCoupon is ERC721Base {
 
-      constructor(
-        string memory _name,
-        string memory _symbol,
-        address _royaltyRecipient,
-        uint128 _royaltyBps
-    )
-        ERC721Base(
-            _name,
-            _symbol,
-            _royaltyRecipient,
-            _royaltyBps
-        )
-    {}
+    mapping(uint => string) internal couponMapping;
 
+    constructor(address _royaltyRecipient, uint128 _royaltyBps) ERC721Base("FlipCoupon2", "FKCPN2", _royaltyRecipient, _royaltyBps){}
+
+    function setCouponCode(uint tokenId, string memory coupon) public {
+        require(msg.sender == owner(), 'coupon code can only be set by owner');
+        couponMapping[tokenId] = coupon;
+    }
+
+    function getCouponCode(uint tokenId) public view returns(string memory){
+        require(bytes(couponMapping[tokenId]).length > 0 , "no coupon created for this token");
+        return couponMapping[tokenId];
+    }
+
+    function burn(uint256 tokenId) external override {
+        bool approvalCheck = msg.sender != owner();
+        _burn(tokenId, approvalCheck);
+    }
 }
